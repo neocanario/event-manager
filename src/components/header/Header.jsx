4b8dc4e72d5ca8fef91efcb6fb10a291879f5a19
neocanario <joel.sanchez.diaz2007@gmail.com>
2026-03-09 17:08:07 +0000

    Añadido menu hamburguesa y actualizacion del readme

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,53 @@
+import { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-import { FaBell, FaEnvelope, FaUser, FaCalendarAlt, FaRss } from 'react-icons/fa';
+import { FaBell, FaEnvelope, FaUser, FaCalendarAlt, FaRss, FaBars, FaTimes } from 'react-icons/fa';
 
 export function Header () {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const closeMenu = () => setIsMenuOpen(false);
+
     return (
         <header>
             <div className='header-logo'>
-                <Link to="/" className='logo-link'>
+                <Link to="/" className='logo-link' onClick={closeMenu}>
                     <FaCalendarAlt className='logo-icon' />
                     <span className='logo-text'>Event Manager</span>
                 </Link>
             </div>
-            <div className='navbar'>
+
+            <button
+                className='hamburger-btn'
+                onClick={() => setIsMenuOpen(prev => !prev)}
+                aria-label={isMenuOpen ? 'Cerrar menú' : 'Abrir menú'}
+            >
+                {isMenuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+
+            <div className={`navbar ${isMenuOpen ? 'is-open' : ''}`}>
                 <nav>
                     <ul className='header-nav-list'>
                         <li>
-                            <Link to="/events" className='nav-link'>
+                            <Link to="/events" className='nav-link' onClick={closeMenu}>
                                 <FaBell className='nav-icon' />
                                 <span>Notificaciones</span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/events" className='nav-link'>
+                            <Link to="/events" className='nav-link' onClick={closeMenu}>
                                 <FaEnvelope className='nav-icon' />
                                 <span>Mensajes</span>
                             </Link>
                         </li>
                         <li className='user-profile'>
-                            <Link to="/finances" className='nav-link'>
+                            <Link to="/finances" className='nav-link' onClick={closeMenu}>
                                 <FaUser className='nav-icon' />
                                 <span>Usuarios</span>
                             </Link>
                         </li>
                         <li>
-                            <Link to="/rss" className='nav-link'>
+                            <Link to="/rss" className='nav-link' onClick={closeMenu}>
                                 <FaRss className='nav-icon rss-icon' />
                                 <span>RSS</span>
                             </Link>
